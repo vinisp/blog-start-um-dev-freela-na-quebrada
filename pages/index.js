c7fea8e2287d5a108532149859e7a12b3a10207b
vinisp <aragao93.vinicius@gmail.com>
2022-05-18 12:50:55 -0300

    comecao com esse framework porreta

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostData }) {
+  return (
+    <Layout home>
+      <div className="container">
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>
+            Dicas para desenvolver lojas e plataformas com ReactJS, React Native
+            e NextJs.
+          </p>
+          <p>Aqui vai um paragrafo explicando o que aconteceu</p>
+        </section>
+      </div>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostData = getSortedPostsData();
+  return {
+    props: {
+      allPostData,
+    },
+  };
+}
